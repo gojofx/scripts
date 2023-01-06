f6c41460957157a784372adc9d6e3ff47be3fb5e
gojofx <112127410+gojofx@users.noreply.github.com>
2023-01-06 00:23:19 -0500

    free download audio in soundsnap
    download audios for free from the page https://www.soundsnap.com/ with this script, paste it in the console and after playing the audios in order you can download them from the same button on the page.

--- scriptSounSnap.js
+++ scriptSounSnap.js
@@ -1,7 +1,11 @@
 let span = document.querySelectorAll('span.ojoo-button');
+let download = document.querySelectorAll('a.ss_download');
+let links = undefined;
+
 for (let i = 0; i < span.length ; i++) {
-    span[i].addEventListener('mouseover', e=>{
-        let links = document.querySelectorAll('audio');
-        console.log(links[0]);
+    span[i].addEventListener('click', e=>{
+        links = (document.querySelectorAll('audio'));
+        let link = "https://" + window.location.host + links[i].getAttribute("src");
+        download[i].setAttribute("href", link );        
     });
 }
